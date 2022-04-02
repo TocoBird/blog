c41f316f3f2209df558af5a7e87541e0defe7c0e
bokotomo <bokotomo@me.com>
2022-04-02 13:34:32 +0900

    feat

--- cmsapi/README.md
+++ cmsapi/README.md
@@ -1,3 +1,14 @@
 # strapi
 
-headless CMS
+headless CMS.  
+(can upload to S3.)  
+https://docs.strapi.io/developer-docs/latest/getting-started/introduction.html
+
+## aws memo
+
+1. create user with IAM.
+2. create s3 bucket and set policy.
+
+## graphql
+
+http://localhost:1337/graphql
--- cmsapi/config/plugins.js
+++ cmsapi/config/plugins.js
@@ -1,14 +1,26 @@
 module.exports = ({ env }) => ({
   upload: {
     config: {
-      provider: 'aws-s3',
+      provider: "aws-s3",
       providerOptions: {
-        accessKeyId: env('AWS_ACCESS_KEY_ID'),
-        secretAccessKey: env('AWS_ACCESS_SECRET'),
-        region: env('AWS_REGION'),
+        accessKeyId: env("AWS_ACCESS_KEY_ID"),
+        secretAccessKey: env("AWS_ACCESS_SECRET"),
+        region: env("AWS_REGION"),
         params: {
-          Bucket: env('AWS_BUCKET')
+          Bucket: env("AWS_BUCKET"),
         },
+      },
+    },
+  },
+  graphql: {
+    config: {
+      endpoint: "/graphql",
+      shadowCRUD: true,
+      playgroundAlways: false,
+      depthLimit: 7,
+      amountLimit: 100,
+      apolloServer: {
+        tracing: false,
       },
     },
   },
