02bb7a657876f47357b25bc7448693910719ffb0
bokotomo <bokotomo@me.com>
2022-06-01 19:47:35 +0900

    feat: remove offline

--- front/gatsby-config.js
+++ front/gatsby-config.js
@@ -80,8 +80,5 @@ module.exports = {
       },
     },
     'gatsby-plugin-sitemap',
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
   ],
 };
