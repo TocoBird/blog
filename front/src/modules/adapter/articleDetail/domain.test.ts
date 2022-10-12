76b09903e75c3ae4c9b6a13c18c487149e5c76a1
bokotomo <bokotomo@me.com>
2022-10-12 22:43:25 +0900

    feat: unit test

--- front/src/modules/adapter/articleDetail/domain.test.ts
+++ front/src/modules/adapter/articleDetail/domain.test.ts
@@ -1,8 +1,12 @@
-import { getDomainCategory } from '@/modules/adapter/articleDetail/domain';
+import {
+  getDomainCategory,
+  getDomainRelatedBlog,
+} from '@/modules/adapter/articleDetail/domain';
 import { DomainArticleDetailCategory } from '@/domain/articleDetail/category';
+import { DomainArticleDetailRecommendBlog } from '@/domain/articleDetail/recommendBlog';
 
 describe('Adapter/ArticleDetail/Domain', () => {
-  test('ドメインに変換される/カテゴリ', () => {
+  test('ドメインに変換できる/カテゴリ', () => {
     const res = [
       {
         id: 1,
@@ -21,6 +25,64 @@ describe('Adapter/ArticleDetail/Domain', () => {
     const result = [
       new DomainArticleDetailCategory(1, 'name1'),
       new DomainArticleDetailCategory(2, 'name2'),
+    ];
+    expect(result).toEqual(domain);
+  });
+
+  test('ドメインに変換できる/おすすめ記事', () => {
+    const res = [
+      {
+        id: 1,
+        attributes: {
+          urlid: 'urlid1',
+          mainTitle: 'title',
+          thumbnail: {
+            data: {
+              attributes: {
+                url: 'url',
+                formats: {
+                  small: {
+                    url: 'smallUrl',
+                  },
+                },
+              },
+            },
+          },
+        },
+      },
+    ];
+    const domain = getDomainRelatedBlog(res);
+    const result = [
+      new DomainArticleDetailRecommendBlog(1, 'urlid1', 'title', 'smallUrl'),
+    ];
+    expect(result).toEqual(domain);
+  });
+
+  test('ドメインに変換できる。small画像がない場合は大きい画像を表示/おすすめ記事', () => {
+    const res = [
+      {
+        id: 1,
+        attributes: {
+          urlid: 'urlid1',
+          mainTitle: 'title',
+          thumbnail: {
+            data: {
+              attributes: {
+                url: 'bigurl',
+                formats: {
+                  small: {
+                    url: '',
+                  },
+                },
+              },
+            },
+          },
+        },
+      },
+    ];
+    const domain = getDomainRelatedBlog(res);
+    const result = [
+      new DomainArticleDetailRecommendBlog(1, 'urlid1', 'title', 'bigurl'),
     ];
     expect(result).toEqual(domain);
   });
